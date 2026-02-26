d60845478e689dbc69d1ea6ad1098984c66773a4
Christian Falch <christian.falch@gmail.com>
2026-02-26 08:04:18 +0100

    Initial Commit

---
+++ MyToday/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyToday
+//
+//  Created by Christian Falch on 26/02/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyToday/MyTodayApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyTodayApp.swift
+//  MyToday
+//
+//  Created by Christian Falch on 26/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct MyTodayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
